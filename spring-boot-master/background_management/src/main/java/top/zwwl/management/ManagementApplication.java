73e74f61556ad7d38353945f3c606b48019d74e6
zhanwangweilai <m17194576883@163.com>
2019-01-11 18:02:52 +0800

    spring+ssm+druid+HTML页面访问+热部署+thymeleaf+HTML页面+后台管理系统初始化项目

---
+++ spring-boot-master/background_management/src/main/java/top/zwwl/management/ManagementApplication.java
@@ -0,0 +1,14 @@
+package top.zwwl.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementApplication.class, args);
+    }
+
+}
+
